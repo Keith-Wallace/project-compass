8153bd4eaba0352b42bf4e9a85294fda42527470
Keith Wallace <keith.t.wallace@gmail.com>
2026-09-18 11:39:04 -0400

    add `PostCSS` and wrap application w/ `MantineProvider`

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createTheme, MantineProvider } from '@mantine/core';
 import App from './app/App'
 
+import '@mantine/core/styles.css';
+import './index.css'
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
+
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <MantineProvider theme={theme}>
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  </MantineProvider>
+);
